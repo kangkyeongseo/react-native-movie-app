32011da497221a5e7d97d68701eac4322c8eb700
kangkyeongseo <kks_big@naver.com>
2022-12-06 16:57:25 +0900

    Preload Using Hook

--- App.js
+++ App.js
@@ -2,36 +2,14 @@ import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
 import { Image } from "react-native";
 
-const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-
-const loadImage = (images) =>
-  images.map((image) => {
-    if (typeof image === "string") {
-      return Image.prefetch(image);
-    } else {
-      return Asset.loadAsync(image);
-    }
-  });
-
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    const fonts = loadFonts([Ionicons.font]);
-    const images = loadImage([require("./image.jpeg")]);
-    await Promise.all([...fonts, ...images]);
-  };
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      />
-    );
+  const [asset] = useAssets([require("./image.jpeg")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
+  if (!asset || !loaded) {
+    return <AppLoading />;
   }
   return null;
 }
